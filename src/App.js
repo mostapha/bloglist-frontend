09342e43c97580ac8870d621345bea2c560dbfca
Mustapha Bouh <67738751+mostapha@users.noreply.github.com>
2023-03-06 22:28:12 +0100

    5.2

--- src/App.js
+++ src/App.js
@@ -6,15 +6,24 @@ import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-
   const [user, setUser] = useState(null)
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const LOCAL_STORAGE_KEY = 'authenticatedUser'
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs(blogs)
     )
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem(LOCAL_STORAGE_KEY)
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
   }, [])
 
   if (user === null) {
@@ -32,7 +41,7 @@ const App = () => {
 
       console.log('loginResponse', login_response)
       setUser(login_response)
-
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(login_response))
     }
 
     return (
@@ -59,10 +68,16 @@ const App = () => {
     )
   }
 
+  const handleLogout = () => {
+    setUser(null)
+    localStorage.removeItem(LOCAL_STORAGE_KEY)
+  }
+
   return (
     <div>
       <h2>blogs</h2>
-      <p>User {user.name} is logged in</p>
+      <p>User {user.name} is logged in. <button onClick={handleLogout}>logout</button></p>
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
