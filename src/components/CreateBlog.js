dac17a9d4ad9b4718423fe0be1ee05a55867cd7d
Mustapha Bouh <67738751+mostapha@users.noreply.github.com>
2023-03-08 13:54:23 +0100

    5.5

--- src/components/CreateBlog.js
+++ src/components/CreateBlog.js
@@ -1,12 +1,14 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import FormInput from './FormInput'
 import blogService from '../services/blogs'
+import Togglable from './Togglable'
 
 const CreateBlog = ({ setNotification }) => {
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
   const [url, setUrl] = useState('')
 
+  const addBlogFormRef = useRef()
 
   const handleBlogCreation = async (event) => {
     event.preventDefault()
@@ -27,18 +29,22 @@ const CreateBlog = ({ setNotification }) => {
     setUrl('')
     console.log('blog create result', response)
     setNotification(`a new blog is added (${response.title} By ${response.author})`)
+
+    addBlogFormRef.current.toggleVisibility()
   }
 
   return (
-    <section>
-      <h2>create a new blog</h2>
-      <form onSubmit={handleBlogCreation}>
-        <FormInput label='title' value={title} onChange={event => {setTitle(event.target.value)}}/>
-        <FormInput label='author' value={author} onChange={event => {setAuthor(event.target.value)}}/>
-        <FormInput label='url' value={url} onChange={event => {setUrl(event.target.value)}}/>
-        <button type='submit'>create</button>
-      </form>
-    </section>
+    <Togglable buttonLabel='new blog' ref={addBlogFormRef}>
+      <section>
+        <h2>create a new blog</h2>
+        <form onSubmit={handleBlogCreation}>
+          <FormInput label='title' value={title} onChange={event => {setTitle(event.target.value)}}/>
+          <FormInput label='author' value={author} onChange={event => {setAuthor(event.target.value)}}/>
+          <FormInput label='url' value={url} onChange={event => {setUrl(event.target.value)}}/>
+          <button type='submit'>create</button>
+        </form>
+      </section>
+    </Togglable>
   )
 }
 
